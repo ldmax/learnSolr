eb892c5d2484e692046e0fc39e2ac4c9b9332e9b
tempovoid\lidanmax <tempovoid@gmail.com>
2017-03-03 18:00:03 +0800

    update

--- learn/src/main/java/com/lidan/learn/controller/RichTextController.java
+++ learn/src/main/java/com/lidan/learn/controller/RichTextController.java
@@ -22,7 +22,9 @@ public class RichTextController {
 	
 	/**
 	 * 用户在输入框输入输入查询关键字，在这里自动匹配常用的fieldName，并给予权重
-	 * 
+	 * 这里我们采用常见的搜索逻辑，即：当搜索框内输入一个词时，默认这个词可能是针对schema已定义
+	 * 的所有field进行搜索，对搜索结果取并集（要去重吗？！）；当搜索框内输入多个词时
+	 * 默认是各词的搜索结果取交集。
 	 * */
 	@RequestMapping(value="/toSearch", method=RequestMethod.GET)
 	public String queryRichTextDocument(String searchInput, Model model){
